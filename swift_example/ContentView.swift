1b41c41a0ace5272b074abda9dd8d8c4b5c4fd18
faraz <kfaraz23@gmail.com>
2024-08-04 18:29:40 +0530

    Initial Commit

---
+++ swift_example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift_example
+//
+//  Created by Faraz on 04/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift_example/swift_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_exampleApp.swift
+//  swift_example
+//
+//  Created by Faraz on 04/08/24.
+//
+
+import SwiftUI
+
+@main
+struct swift_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
