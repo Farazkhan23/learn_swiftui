00f38783517ae8f8cd6fbece2df7cab2bbc55835
faraz <kfaraz23@gmail.com>
2024-08-04 19:04:00 +0530

    Landmark example

---
+++ swift_example/CircleImage.swift
@@ -0,0 +1,18 @@
+//
+//  CircleImage.swift
+//  swift_example
+//
+//  Created by Faraz on 04/08/24.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
